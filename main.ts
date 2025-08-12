680ad30b39d18b66f5f6b00a11b9414ab53b0f20
Grace Mihiral <mihiralgrace@gmail.com>
2025-08-12 13:33:59 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -46,7 +46,7 @@ Class: Mage
 	
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new RPGSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -195,7 +195,7 @@ Class: Mage
 }
 
 
-class SampleSettingTab extends PluginSettingTab {
+class RPGSettingTab extends PluginSettingTab {
 	plugin: RPGPlugin;
 
 	constructor(app: App, plugin: RPGPlugin) {
